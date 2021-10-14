4bd0d15cbbdfd627eeb69573629a0f26cee0e9dc
Ina <iandersin@gmail.com>
2021-10-15 00:39:54 +0300

    guildMemberAdd

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,24 @@
 import * as dotenv from "dotenv";
+import {Client, Intents, Permissions, TextChannel} from "discord.js";
 dotenv.config();
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS] });
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+//New member greeting
+client.on("guildMemberAdd", (guildMember) => {
+    let guildChannel = guildMember.guild.channels.cache.get('860896985045401630');
+    let newMemberID = guildMember.id;
+    if (guildChannel?.isText()) {
+        (guildChannel as unknown as TextChannel).send(`Welcome, <@${newMemberID}>!`);
+    }
+
+
+});
+
+
+
+client.login(process.env.BOT_TOKEN);
