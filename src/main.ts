6fe599bce3eff7822be69433f0b924fefef26afb
Ina <iandersin@gmail.com>
2022-08-19 16:15:56 +0300

    ts version update, slash commands

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import * as dotenv from "dotenv";
 import {Client, Intents, TextChannel} from "discord.js";
 import raidInfo from "./raidInfo.json";
-import {scheduleJob} from "node-schedule";
+import reminders from "./reminders.json"
+import {scheduledJobs, scheduleJob} from "node-schedule";
 
 let envPath = "./.env";
 
@@ -17,8 +18,8 @@ client.once('ready', () => {
     console.log('Ready!');
 });
 
-let scheduledJobs = raidInfo.map(raidDay => scheduleJob(raidDay.jobSchedule, ()=> raidDayReminder(raidDay.image, raidDay.message)));
-
+let scheduledJobsRaids = raidInfo.map(raidDay => scheduleJob(raidDay.jobSchedule, ()=> raidDayReminder(raidDay.image, raidDay.message)));
+let scheduledJobsReminders = reminders.map(reminders => scheduleJob(reminders.jobSchedule, ()=> fcReminders(reminders.message)));
 
 function raidDayReminder(imageToPost: string, messageToPost: string){
     client.guilds.cache.forEach(guild => {
@@ -29,6 +30,16 @@ function raidDayReminder(imageToPost: string, messageToPost: string){
         }
     })
 }
+
+function fcReminders(messageToPost: string){
+    client.guilds.cache.forEach(guild => {
+        let generalChatChannel = guild.channels.cache.get(`${process.env.CHAT_CHANNEL}`);
+        if (generalChatChannel?.isText()) {
+            generalChatChannel.send({content: `${messageToPost}`}).then(()=> console.log("Reminder done."));
+        }
+    })
+}
+
 
 
 const kickJob = scheduleJob(`${process.env.SCHEDULE_GUEST_KICK_JOB}`, function (){
@@ -77,15 +88,82 @@ client.on("guildMemberRemove", (guildMember)=>{
     }
 });
 
+client.on('interactionCreate', async interaction => {
+    //check interaction type
+    if(!interaction.isCommand()) return;
 
+    if (interaction.commandName === 'addcactpot') {
+        if (!process.env.CACTPOT_ROLE) {
+            console.log('Cactpot role error.');
+            return interaction.reply('Internal error.');
+        }
+        if (!interaction.guild) {
+            console.log('Guild error.')
+            return interaction.reply('Internal error.');
+        }
+
+        let guildMember = interaction.guild.members.cache.get(interaction.user.id);
+
+        if (!guildMember) {
+            console.log('Member not found.');
+            return interaction.reply('Member not found.');
+        }
+
+        if (!guildMember.roles.cache.get(process.env.CACTPOT_ROLE)) {
+            let role = guildMember.guild.roles.cache.get(`${process.env.CACTPOT_ROLE}`)
+            if (role) {
+                await guildMember.roles.add(role);
+                await interaction.reply('You now have the Cactpot role!');
+            } else {
+                return interaction.reply('Role not found.');
+            }
+        } else {
+            return interaction.reply('You already have the Cactpot role.');
+        }
+    }
+
+    if (interaction.commandName === "removecactpot") {
+        if (!process.env.CACTPOT_ROLE){
+            console.log('Cactpot role error.');
+            return interaction.reply('Internal error.');
+        }
+
+        if (!interaction.guild) {
+            console.log('Guild error.')
+            return interaction.reply('Internal error.');
+        }
+
+        let guildMember = interaction.guild.members.cache.get(interaction.user.id);
+
+        if (!guildMember) {
+            console.log('Member not found.');
+            return interaction.reply('Member not found.');
+        }
+
+        if (guildMember.roles.cache.get(process.env.CACTPOT_ROLE)) {
+            let role = guildMember.guild.roles.cache.get(`${process.env.CACTPOT_ROLE}`)
+            if (role) {
+                await guildMember.roles.remove(role);
+                await interaction.reply('You no longer have the Cactpot role!');
+            } else {
+                return interaction.reply('Role not found.');
+            }
+        } else {
+            return interaction.reply('Could not remove role.');
+        }
+    }
+
+});
 
 client.login(process.env.BOT_TOKEN).then(function (){
     console.log('Logged in.');
 });
 
 function shutdown(){
-    scheduledJobs.forEach(job => job.cancel());
+    scheduledJobsRaids.forEach(job => job.cancel());
     console.log("Raid reminder jobs canceled.")
+    scheduledJobsReminders.forEach(job => job.cancel());
+    console.log("FC reminder jobs canceled.")
     kickJob.cancel();
     console.log("Kick job canceled.")
     client.destroy();
